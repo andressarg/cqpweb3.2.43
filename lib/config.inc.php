b08ece9c9c3a2ccb3342a61d0e7d9bf3b58dd43f
Andressa Gomide <gomide.andressa@gmail.com>
2022-10-12 14:55:22 +0200

    some cleaning

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -6,30 +6,3 @@
  * by | with no stray whitespace.      *
  * ----------------------------------- */
 
-$superuser_username = 'grauna';
-
-
-/* -------------------------- *
- * database connection config *
- * -------------------------- */
-
-$mysql_webuser = 'cqpweb_user';
-$mysql_webpass = 'CQPweb_pass22';
-$mysql_schema  = 'cqpweb_db';
-$mysql_server  = 'localhost';
-
-
-
-/* ---------------------- *
- * server directory paths *
- * ---------------------- */
-
-$cqpweb_tempdir   = '/srv/cqpwebdata/cache';
-$cqpweb_uploaddir = '/srv/cqpwebdata/upload';
-$cwb_datadir      = '/srv/cqpwebdata/index';
-$cwb_registry     = '/srv/cqpwebdata/registry';
-
-
-
-
-$sql_local_infile_disabled  = 'true';
